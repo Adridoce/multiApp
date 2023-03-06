0a6d0f504656dd878b59f672f0feb80237438bd8
Adridoce <adrisimancas12@gmail.com>
2023-03-06 01:53:18 +0100

    Prueba

--- app/src/main/java/com/adridev/cursoandroid/superheroapp/ApiService.kt
+++ app/src/main/java/com/adridev/cursoandroid/superheroapp/ApiService.kt
@@ -5,7 +5,7 @@ import retrofit2.http.GET
 import retrofit2.http.Path
 
 interface ApiService {
-
+    // clave api
     @GET("api/5928799777240589/search/{name}")
     suspend fun getSuperHeros(@Path("name") superHeroName:String):Response<SuperHeroDataResponse>
 }
